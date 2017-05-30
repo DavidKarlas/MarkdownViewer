27c0fd3a5b52c60bcaaa6183f67a02700975128d
roman-yagodin <roman.yagodin@gmail.com>
2017-05-30 11:33:28 +0300

    MonoDevelop 6.0 support: - Convert to recent MonoDevelop API (AbstractXwtViewContent) - Migrate to latest AddinMaker version as specified in https://mhut.ch/addinmaker/1.2 - Add Xwt package from NuGet - Add MonoDevelop.Ide assembly from v6.1.2.44 to the project source

--- MarkdownViewer/MarkdownViewer.cs
+++ MarkdownViewer/MarkdownViewer.cs
@@ -1,24 +1,26 @@
-﻿using System;
+﻿using MonoDevelop.Core;
 using MonoDevelop.Ide.Gui;
 using MonoDevelop.Projects.Text;
 using Xwt;
 using Xwt.Drawing;
-using MonoDevelop.Core;
 
 namespace MarkdownViewer
 {
-	public class MarkdownViewer:AbstractBaseViewContent, IAttachableViewContent, IViewContent
+    public class MarkdownViewer: AbstractXwtViewContent
 	{
 		ScrollView scrollView;
-		MarkdownView markdownControl;
-		Document doc;
 
-		public MarkdownViewer (Document doc)
-		{
-			this.doc = doc;
-		}
+        MarkdownView markdownControl;
 
-		public override Gtk.Widget Control {
+        readonly Document doc;
+
+        public MarkdownViewer (Document doc)
+        {
+            this.doc = doc;
+            ContentName = "Markdown";
+        }
+
+        public override Widget Widget {
 			get {
 				if (scrollView == null) {
 					markdownControl = new MarkdownView ();
@@ -29,7 +31,7 @@ namespace MarkdownViewer
 					}
 					markdownControl.Font = markdownControl.Font.WithSize (11);
 					markdownControl.Margin = 30;
-					var frame = new FrameBox (markdownControl);
+                   	var frame = new FrameBox (markdownControl);
 					frame.MinWidth = 800;
 					frame.WidthRequest = 800;
 					frame.HorizontalPlacement = WidgetPlacement.Center;
@@ -40,129 +42,44 @@ namespace MarkdownViewer
 					frame.Margin = 10;
 					scrollView = new ScrollView (frame);
 				}
-				return (Gtk.Widget)Toolkit.CurrentEngine.GetNativeWidget (scrollView);
+				return scrollView;
 			}
 		}
 
-		void IAttachableViewContent.Selected ()
-		{
-			markdownControl.Markdown = doc.GetContent<ITextFile> ().Text;
-		}
-
-		void IAttachableViewContent.Deselected ()
-		{
-		}
-
-		void IAttachableViewContent.BeforeSave ()
-		{
-		}
-
-		void IAttachableViewContent.BaseContentChanged ()
-		{
-		}
-
-		event EventHandler IViewContent.BeforeSave { add { } remove { }
-		}
-
-		event EventHandler IViewContent.ContentChanged { add { } remove { }
-		}
-
-		event EventHandler IViewContent.ContentNameChanged { add { } remove { }
-		}
-
-		event EventHandler IViewContent.DirtyChanged { add { } remove { }
-		}
-
-		void IViewContent.Load (string fileName)
-		{
-		}
-
-		void IViewContent.LoadNew (System.IO.Stream content, string mimeType)
-		{
-		}
-
-		void IViewContent.Save (string fileName)
-		{
-		}
-
-		void IViewContent.Save ()
-		{
-		}
-
-		void IViewContent.DiscardChanges ()
-		{
-		}
-
-		MonoDevelop.Projects.Project IViewContent.Project {
-			get {
-				return null;
-			}
-			set {
-			}
-		}
-
-		string IViewContent.PathRelativeToProject {
-			get {
-				return "";
-			}
-		}
-
-		string IViewContent.ContentName {
-			get {
-				return TabPageLabel;
-			}
-			set { }
-		}
-
-		string IViewContent.UntitledName {
-			get {
-				return "";
-			}
-			set { }
-		}
-
-		string IViewContent.StockIconId {
+        public override string StockIconId {
 			get {
 				return null;
 			}
 		}
 
-		bool IViewContent.IsUntitled {
-			get {
-				return false;
-			}
-		}
-
-		bool IViewContent.IsViewOnly {
+        public override bool IsViewOnly {
 			get {
 				return true;
 			}
 		}
 
-		bool IViewContent.IsFile {
+        public override bool IsFile {
 			get {
 				return false;
 			}
 		}
 
-		bool IViewContent.IsDirty {
-			get {
-				return false;
-			}
-			set { }
-		}
-
-		bool IViewContent.IsReadOnly {
+        public override bool IsReadOnly {
 			get {
 				return true;
 			}
 		}
-
+     
 		public override string TabPageLabel {
 			get {
-				return GettextCatalog.GetString ("Markdown view");
+				return GettextCatalog.GetString (ContentName);
 			}
 		}
+
+        protected override void OnSelected ()
+        {
+        	markdownControl.Markdown = doc.GetContent<ITextFile> ().Text;
+        }
 	}
 }
 
